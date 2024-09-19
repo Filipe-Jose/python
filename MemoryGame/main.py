d2eef317df0a9d2296164576674f7b0e49f8f541
Filipe Jose <filipejosecamargo2014@gmail.com>
2024-09-19 16:57:50 -0300

    Jogo da Memória dia 2

--- MemoryGame/main.py
+++ MemoryGame/main.py
@@ -1,21 +1,36 @@
 from tkinter import *
 import random
-from time import sleep
 
 
 jogo = Tk()
+jogo.title('Jogo da Memória')
+
+menu = Frame(jogo)
+placar = Frame(menu)
+cartas = Frame(jogo)
 
 
 class Carta:
     def __init__(self, foto, nome):
-        self.foto = PhotoImage(file=foto)
+        self.foto = PhotoImage(file=foto).subsample(3, 3).zoom(2, 2)
         self.nome = nome
 
 
 class Player:
-    def __init__(self, cor):
+    def __init__(self, nome, cor):
+        self.nome = nome
         self.cor = cor
         self.pontos = 0
+        self.label = Label(placar, text=(f'{self.nome}: {self.pontos}'), font=('Consolas', 12), fg=self.cor, padx = 100)
+        self.text = lambda: str(f'{self.nome}: {self.pontos}')
+    
+
+class Selecao:
+    def __init__(self, label, linha, coluna, animal):
+        self.label = label
+        self.linha = linha
+        self.coluna = coluna
+        self.animal = animal
 
 
 # criando cartas
@@ -42,8 +57,8 @@ urso = Carta('cartas\\urso.png', 'Urso')
 zebra = Carta('cartas\\zebra.png', 'Zebra')
 
 # criando players
-player1 = Player('#0000FF')
-player2 = Player('#FF0000')
+player1 = Player('Player 1', '#0000FF')
+player2 = Player('Player 2', '#FF0000')
 
 # constantes
 CARTAS = [cachorro, cavalo, coelho, coruja, elefante, gato, girafa, golfinho, jacare, leao, lobo, macaco, passaro,
@@ -53,10 +68,11 @@ ROWS = 6
 COLUMNS = 7
 
 # variaveis
+labels = []
 jogando = None
 baralho = []
-primeira_selecao = None
-segunda_selecao = None
+primeira = Selecao(None, None, None, None)
+segunda = Selecao(None, None, None, None)
 buttons = [['', '', '', '', '', '', ''],
           ['', '', '', '', '', '', ''],
           ['', '', '', '', '', '', ''],
@@ -66,14 +82,22 @@ buttons = [['', '', '', '', '', '', ''],
 
 
 def new_game():
-    global baralho, jogando, PLAYERS, CARTAS, ROWS, COLUMNS, cartas
+    global baralho, jogando, PLAYERS, CARTAS, ROWS, COLUMNS, cartas, primeira, segunda, labels
 
     # reiniciar pontuações
     for player in PLAYERS:
         player.pontos = 0
 
+    # reiniciar tela
+    for i in labels:
+        i.destroy()
+
+    # reiniciar selecoes
+    primeira.label = None
+
     # randomizar primeiro player
     jogando = random.choice(PLAYERS)
+    alternar_jogador()
 
     # embaralhar
     baralho = []  # resetar baralho
@@ -87,44 +111,116 @@ def new_game():
         baralho.append(temp)  # armazenar linha no baralho
 
     # configurar na janela
-    cartas.grid()
+    cartas.pack(side='bottom')
     for linha in range(ROWS):
         for coluna in range(COLUMNS):
-            buttons[linha][coluna] = Button(cartas, height=4, width=9,
-                                          command=lambda: mostrar_carta(baralho[linha][coluna],
-                                                                        linha, coluna))
+            buttons[linha][coluna] = Button(cartas, height = 6, width = 12, command = lambda l = linha, c = coluna: mostrar_carta(baralho[l][c], l, c))
             buttons[linha][coluna].grid(row=linha, column=coluna)
 
 
 def mostrar_carta(carta, linha, coluna):
-    global primeira_selecao, segunda_selecao, cartas
+    global primeira, segunda, cartas
 
-    # fazer carta aparecer
-    buttons[linha][coluna].config(text=carta.nome, image=carta.foto, compound='top')
-    buttons[linha][coluna].grid(row=linha, column=coluna)
-    cartas.grid()
+    # fazer botão desaparecer
+    buttons[linha][coluna].grid_forget()
 
-
-    # configurar seleção
-    if primeira_selecao != None:
-        primeira_selecao = carta.nome
+    # configurar seleção e mostra carta
+    if primeira.animal is None:
+        primeira.animal = carta
+        primeira.linha = linha
+        primeira.coluna = coluna
+        primeira.label = Label(cartas, text=carta.nome, image=carta.foto, compound='top', font=('Consolas', 12))
+        primeira.label.grid(row = linha, column = coluna)
     else:
-        segunda_selecao = carta.nome
-        comparar_cartas()
-
+        segunda.animal = carta
+        segunda.linha = linha
+        segunda.coluna = coluna
+        segunda.label = Label(cartas, text=carta.nome, image=carta.foto, compound='top', font=('Consolas', 12))
+        segunda.label.grid(row = linha, column = coluna)
+        igual = comparar_cartas()
+      
 
 def comparar_cartas():
-    global primeira_selecao, segunda_selecao, jogando, PLAYERS
+    global primeira, segunda, jogando, PLAYERS
 
+    desabilitar_botoes()
+    
     # comparar cartas
-    if primeira_selecao == segunda_selecao:
+    if primeira.animal == segunda.animal:  # se acertou
         jogando.pontos += 1
+        atualizar_placar()
+        primeira.label.config(bd = 2, bg = jogando.cor)  # mudar cor do fundo e colocar borda
+        segunda.label.config(bd = 2, bg = jogando.cor)
+
+        # guardar labels para apagar dps
+        labels.append(primeira.label)
+        labels.append(segunda.label)
+
+        primeira.animal = None
+        segunda.animal = None
+        reabilitar_botoes()
     else:
-        pass
+        cartas.after(2000, esconder_cartas)
+
+    
+def desabilitar_botoes():
+    global ROWS, COLUMNS, buttons
+
+    for linha in range(ROWS):
+        for coluna in range(COLUMNS):
+            buttons[linha][coluna].config(state='disabled')
 
 
-cartas = Frame(jogo)
+def reabilitar_botoes():
+    global ROWS, COLUMNS, buttons
+    
+    for linha in range(ROWS):
+        for coluna in range(COLUMNS):
+            if buttons[linha][coluna]:  # Verifica se o botão ainda existe
+                buttons[linha][coluna].config(state='normal')        
+
+
+def alternar_jogador():
+    global player1, player2, jogando, turno
+
+    if jogando == player1:
+            jogando = player2
+    else:
+            jogando = player1
+    
+    atualizar_placar()
+
+
+def esconder_cartas():
+    global primeira, segunda, jogando
+
+    primeira.label.destroy()
+    primeira.label = None
+    primeira.animal = None
+    buttons[primeira.linha][primeira.coluna].grid(row = primeira.linha, column = primeira.coluna)
+
+    segunda.label.destroy()
+    segunda.label = None
+    segunda.animal = None
+    buttons[segunda.linha][segunda.coluna].grid(row = segunda.linha, column = segunda.coluna)
+
+    alternar_jogador()
+    reabilitar_botoes()
+    
+
+def atualizar_placar():
+    global PLAYERS, jogando, turno
+
+    for i in PLAYERS:
+        i.label.config(text=i.text())
+        i.label.pack()
+
+    jogo.config(background=jogando.cor)
+
+
+placar.pack(side='left')
+menu.pack(side='top')
+Button(menu, text='Embaralhar', font=('Consolas', 12), command=new_game).pack(side='right')
 new_game()
 
 jogo.mainloop()
-
